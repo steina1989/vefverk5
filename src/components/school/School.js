33ec365e85fcc15d1bd0732ac3e73695ee587f67
Steina <steina.dogg@gmail.com>
2018-04-02 20:57:26 +0000

    Api work

--- src/components/school/School.js
+++ src/components/school/School.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import path from 'path';
+import Helmet from 'react-helmet';
 import './School.css';
-
-/**
- * Í þessum component ætti að vera mest um að vera og séð um að:
- * - Sækja gögn fyrir svið og birta
- * - Opna/loka deildum
- */
 
 export default class School extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      school : null
+    }
+  }
+
+  componentDidMount(){
+    const {url} = this.props.match;
+    const api = path.join(process.env.REACT_APP_SERVICE_URL,url);
+    fetch(api).then(resp => resp.json()).then(data =>{
+      this.setState({school : data.school})
+    })
+  }
+
   render() {
+
+    if (!this.state.school) return <h1>Sæki gögn...</h1>
+
+    
 
     return (
       <section className="school">
-        <p>útfæra</p>
+        <Helmet title={this.state.school.heading} />
+        <h1>{this.state.school.heading}</h1>
+
       </section>
     );
   }
